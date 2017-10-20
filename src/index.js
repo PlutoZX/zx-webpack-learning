02f28e8eb303d20b96b507f356f66ac47913eb80
zhaoxuan <zhaoxuan1202@163.com>
2017-10-20 17:37:07 +0800

    start

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import _ from 'lodash';
+
+import './style.css';
+
+function component() {
+    var element = document.createElement('div');
+
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    return element;
+}
+
+document.body.appendChild(component());
