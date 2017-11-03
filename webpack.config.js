04613f2460c1a403acb69a73ed0475653fedbce6
zhaoxuan <zhaoxuan1202@163.com>
2017-11-03 21:12:01 +0800

    extract css one

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,7 @@
+const env = process.env.NODE_ENV;
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+
 module.exports = {
     devtool: "inline-source-map",
     devServer: {
@@ -24,26 +28,46 @@ module.exports = {
                 test: /\.css$/,
                 // use: ['style-loader','css-loader'] // 这种简单的写成loader名数组的方式虽然简单但是有个问题，不能对单独的loader进行配置
                 // use: ['css-loader', 'style-loader'] // 这个就会报错说明，loader加载器有顺序之分
-                use: [
-                    {
-                        loader: 'style-loader'
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: true, // 启用css module
-                            localIdentName: '[path][name]__[local]-[hash:base64:5]', // css module 中类名的命名格式，path是该css文件所在目录，name是css文件名，local是该类名在css文件中的本名，hash是hash值
-                            minimize: true,
-                            sourceMap: true
+                use: env === 'production'
+                    ? ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: [
+                            {
+                                loader: 'css-loader',
+                                options: {
+                                    modules: true, // 启用css module
+                                    localIdentName: '[path][name]__[local]-[hash:base64:5]', // css module 中类名的命名格式，path是该css文件所在目录，name是css文件名，local是该类名在css文件中的本名，hash是hash值
+                                    minimize: true,
+                                    sourceMap: true
+                                }
+                            }
+                        ]
+                    })
+                    : [
+                        {
+                            loader: 'style-loader'
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true, // 启用css module
+                                localIdentName: '[path][name]__[local]-[hash:base64:5]', // css module 中类名的命名格式，path是该css文件所在目录，name是css文件名，local是该类名在css文件中的本名，hash是hash值
+                                minimize: true,
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'postcss-loader'
                         }
-                    },
-                    {
-                        loader: 'postcss-loader'
-                    }
-                ]
+                    ]
             }
         ]
-    }
-
-
+    },
+    plugins: env === 'production'
+        ? [
+            new ExtractTextPlugin({
+                filename: '[name].css'
+            })
+        ]
+        : []
 };
